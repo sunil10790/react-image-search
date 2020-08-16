b7f866de8f29da5c85cf0338c1336f1e227cba77
Sunil Kumar C <sunil@Sunils-MacBook-Air.local>
2020-08-16 12:25:34 +0530

    Get height of the image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -7,6 +7,14 @@ class ImageCard extends React.Component {
     this.imageRef = React.createRef();
   }
 
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return <img ref={this.imageRef} alt={description} src={urls.regular} />;
